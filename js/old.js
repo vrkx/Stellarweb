39b0ddf608a57cdba91574468c125d7e204e912a
vrkx <141280206+vrkx@users.noreply.github.com>
2025-01-31 16:06:00 -0600

    Added Old News, Better Project Organization.

---
+++ js/old.js
@@ -0,0 +1,48 @@
+async function fetchData() {
+    try {
+        const response = await fetch('/json/Old.json');
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        displayData(data);
+    } catch (error) {
+        console.error('There was a problem with the fetch operation:', error);
+    }
+}
+
+
+function displayData(data) {
+
+    const itemsContainer = document.getElementById('oitems');
+    data.items.forEach(item => {
+        const box = document.createElement('div');
+        
+        if (item.isold) {
+// Set the opacity of the target element to 0.5
+box.style.opacity = "0.4";
+} else {
+// Optional: Reset opacity if isOld is false
+box.style.opacity = "1";
+}
+
+        box.className = 'box';
+        box.style.backgroundColor = item.color;
+
+        const image = document.createElement('img');
+        image.src = item.image;
+        image.alt = item.description;
+
+        
+
+        const content = document.createElement('div');
+        content.innerHTML = `<h2>${item.Title}</h2><p>${item.description}</p> <p class="date">${item.date}</p>`;
+        
+        box.appendChild(image);
+        box.appendChild(content);
+        itemsContainer.appendChild(box);
+    });
+}
+
+
+fetchData();
