5a4726e390132c8205db484c90bcaf05932cad88
vrkx <141280206+vrkx@users.noreply.github.com>
2025-02-01 07:01:41 -0600

    ??

--- new.js
+++ new.js
@@ -1,6 +1,7 @@
+
 async function fetchData() {
     try {
-        const response = await fetch('/Stellarweb/json/Old.json');
+        const response = await fetch('News.json');
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
@@ -14,18 +15,9 @@ async function fetchData() {
 
 function displayData(data) {
 
-    const itemsContainer = document.getElementById('oitems');
+    const itemsContainer = document.getElementById('items');
     data.items.forEach(item => {
         const box = document.createElement('div');
-        
-        if (item.isold) {
-// Set the opacity of the target element to 0.5
-box.style.opacity = "0.4";
-} else {
-// Optional: Reset opacity if isOld is false
-box.style.opacity = "1";
-}
-
         box.className = 'box';
         box.style.backgroundColor = item.color;
 
@@ -33,8 +25,8 @@ box.style.opacity = "1";
         image.src = item.image;
         image.alt = item.description;
 
-        
 
+        
         const content = document.createElement('div');
         content.innerHTML = `<h2>${item.Title}</h2><p>${item.description}</p> <p class="date">${item.date}</p>`;
         
--- old.js
+++ old.js
@@ -1,7 +1,6 @@
-
 async function fetchData() {
     try {
-        const response = await fetch('/Stellarweb/json/News.json');
+        const response = await fetch('Old.json');
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
@@ -15,9 +14,18 @@ async function fetchData() {
 
 function displayData(data) {
 
-    const itemsContainer = document.getElementById('items');
+    const itemsContainer = document.getElementById('oitems');
     data.items.forEach(item => {
         const box = document.createElement('div');
+        
+        if (item.isold) {
+// Set the opacity of the target element to 0.5
+box.style.opacity = "0.4";
+} else {
+// Optional: Reset opacity if isOld is false
+box.style.opacity = "1";
+}
+
         box.className = 'box';
         box.style.backgroundColor = item.color;
 
@@ -25,8 +33,8 @@ function displayData(data) {
         image.src = item.image;
         image.alt = item.description;
 
-
         
+
         const content = document.createElement('div');
         content.innerHTML = `<h2>${item.Title}</h2><p>${item.description}</p> <p class="date">${item.date}</p>`;
         
